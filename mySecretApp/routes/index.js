6d3dd1ac9be48c363b958f696f7c0bfd29b16f52
Claire Murray <clairefromcork@gmail.com>
2016-02-16 20:00:31 +0000

    Was getting an error
    Got a page not found error on the hello page I added, took me a while to figure out what it was, but I had just forgotten to put in the /

--- mySecretApp/routes/index.js
+++ mySecretApp/routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('hello', function(req, res, next) {
+router.get('/hello', function(req, res, next) {
   res.render('hello', { title: 'Hello World' });
 });
 module.exports = router;
